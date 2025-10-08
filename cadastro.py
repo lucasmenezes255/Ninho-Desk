ff53fb9ba97a09006fc0d4eef8676acd6e5a8e23
Lucas Menezes <lucasmenezes.santos56@gmail.com>
2025-10-08 00:03:35 -0300

    Desenvolvimento inicial

--- cadastro.py
+++ cadastro.py
@@ -1,6 +1,5 @@
 import json
 import os
-
 def carregar_dados():
     if not os.path.exists('dados_usuarios.json'): # Em caso de não existir o arquivo, retorna um dicionário vazio
         return {}
@@ -13,20 +12,26 @@ def cadastrar_usuario():
     email = str(input('Digite seu email: '))
     senha = str(input('Digite sua senha: '))
     serie_crianca = str(input('Digite a série da criança: '))
-    dados_usuarios = carregar_dados() # Carrega os dados existentes para dados_string
+    dados_usuarios =  carregar_dados() # Carrega os dados existentes para dados_usuarios
     dados_usuarios[email] = {'Nome': nome, 'Série da Criança': serie_crianca, 'Senha': senha} # O email cadastrado é a chave dos dados novos
-    with open('dados.json', 'w', encoding='utf-8') as arquivo:
+    with open('dados_usuarios.json', 'w', encoding='utf-8') as arquivo:
         json.dump(dados_usuarios, arquivo, indent=4, ensure_ascii=False)
 
-cadastrar_usuario()
+def redefinir_senha(email):
+    dados = carregar_dados()
+    nova_senha = str(input('Digite uma nova senha: '))
+    dados[email]['Senha'] = nova_senha
+    with open('dados_usuarios.json', 'w', encoding='utf-8') as arquivo:
+        json.dump(dados, arquivo, indent=4, ensure_ascii=False)
+    print('Senha redefinida com sucesso')
 
 '''
 Aqui tá somente o fluxo principal. Falta tratar os erros e validações:
-- Verificar se já existe email cadastrado
+- Verificar se já existe email cadastrado (FEITO)
 - Verificar campo vazio
 - Verificar se no campo de inserir o nome o usuário não digitou um número ou caractere especial
 - Verificar se o email digitado é válido
-- Confirmação de senha
+- Confirmação de senha (FEITO)
 - Verificar se a senha tem no mínimo 8 carac
 - Verificar se a série da criança é um número válido (1 a 9)
 - Esconder a senha digitada (Eu achei a biblioteca getpass, talvez funcione)
---
+++ main.py
@@ -0,0 +1,69 @@
+from cadastro import cadastrar_usuario, carregar_dados, redefinir_senha
+
+def tracinho():
+    print('-'*40)
+
+def menu_estudante():
+    print('Menu')
+
+def verifica_email(email): 
+    dados = carregar_dados()   
+    if not email in dados:
+        print('Email inválido! Cadastre um novo usuário ou insira um email válido.')
+        while True:
+            tracinho()
+            print('[1] Inserir um email válido')
+            print('[2] Cadastrar um novo usuário')
+            tracinho()
+            escolha = int(input('Esolha uma opção acima: '))
+            if  escolha == 1:
+                email = str(input('Insira um email válido: '))
+                verifica_email(email)
+                break
+            elif escolha == 2:
+                cadastrar_usuario()
+                break                                             
+            else:
+                print('Opção inválida! Tente novamente!')
+                break
+
+def verifica_senha(email, senha):
+    dados = carregar_dados()
+    contador = 0
+    while contador < 4:
+        if dados[email]['Senha'] == senha:
+            menu_estudante()
+            break
+        else:
+            print('Senha incorreta, tente novamente!')
+            senha = str(input('Informe a sua senha: '))
+            contador += 1
+        if contador == 4:
+            tracinho()
+            print('Quantidade de tentativas expirada, crie uma nova senha!')
+            tracinho()
+            redefinir_senha(email)
+
+
+def login():
+    dados = carregar_dados()
+    email = str(input('Informe o email de login: '))
+    verifica_email(email)
+    while True:
+        print('[1] Informe a senha')
+        print('[2] Esqueceu a senha')
+        tracinho()
+        escolha = int(input('Esolha uma opção acima: '))
+        if escolha == 1:
+            senha = str(input('Informe a sua senha: '))
+            tracinho()
+            verifica_senha(email, senha)
+            break
+        elif escolha == 2:
+            tracinho()
+            redefinir_senha(email)
+            break                                             
+        else:
+            print('Opção inválida! Tente novamente!')
+        
+login()
